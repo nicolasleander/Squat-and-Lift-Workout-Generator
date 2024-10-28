9ed770dbdb7367ffd18e57f0de285e9708a18056
Nicolas Leander Bugaer <nicolas@bugaer.com>
2024-10-28 15:14:13 +0100

    update squat.rb

--- squat.rb
+++ squat.rb
@@ -1,3 +1,6 @@
+require 'date'
+require 'securerandom'
+
 class WorkoutGenerator
   attr_reader :week, :rm, :exercises
 
@@ -8,7 +11,7 @@ class WorkoutGenerator
   end
 
   def generate_workout
-    puts "Week #{week} - Workout Schedule"
+    puts "Week #{week} - Olympic Weightlifting Program"
     exercises.each do |day|
       print_day_schedule(day)
     end
@@ -68,9 +71,76 @@ class WorkoutGenerator
   end
 
   def print_workout_details(workout)
-    weight_str = workout[:weight].is_a?(Range) ? "#{workout[:weight].min}kg - #{workout[:weight].max}kg" : "#{workout[:weight]}kg"
+    weight_str = workout[:weight].is_a?(Range) ?
+      "#{workout[:weight].min}kg - #{workout[:weight].max}kg" :
+      "#{workout[:weight]}kg"
     puts "  #{workout[:name]}: #{workout[:type]} @ #{weight_str}, #{workout[:sets]} sets of #{workout[:reps]}"
   end
 end
 
-WorkoutGenerator.new(1).generate_workout
+class ICSGenerator
+  DAYS_OF_WEEK = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
+
+  def self.generate(workouts)
+    # Find next Monday
+    next_monday = Date.today + ((8 - Date.today.wday) % 7)
+
+    calendar = []
+    calendar << "BEGIN:VCALENDAR"
+    calendar << "VERSION:2.0"
+    calendar << "PRODID:-//Olympic Weightlifting Program//Calendar//EN"
+    calendar << "CALSCALE:GREGORIAN"
+
+    workouts.exercises.each do |day|
+      next if day[:workouts].empty?
+
+      day_num = DAYS_OF_WEEK.index(day[:day])
+      date = next_monday + day_num
+
+      # Format workout description
+      description = format_description(day[:workouts])
+
+      calendar << "BEGIN:VEVENT"
+      calendar << "UID:#{SecureRandom.uuid}"
+      calendar << "DTSTAMP:#{DateTime.now.strftime("%Y%m%dT%H%M%SZ")}"
+      calendar << "DTSTART:#{date.strftime("%Y%m%d")}T140000"
+      calendar << "DTEND:#{date.strftime("%Y%m%d")}T150000"
+      calendar << "SUMMARY:Olympic Lifting - #{day[:day]}"
+      calendar << "DESCRIPTION:#{description}"
+      calendar << "LOCATION:Weightlifting Gym"
+      calendar << "END:VEVENT"
+    end
+
+    calendar << "END:VCALENDAR"
+
+    File.write('olympic_lifting_program.ics', calendar.join("\r\n"))
+  end
+
+  private
+
+  def self.format_description(workouts)
+    lines = ["🏋️ Olympic Weightlifting Program\\n\\n"]
+
+    workouts.each do |w|
+      weight = w[:weight].is_a?(Range) ? "#{w[:weight].min}-#{w[:weight].max}kg" : "#{w[:weight]}kg"
+
+      lines << "#{w[:name].upcase}\\n"
+      lines << "➡️ Intensity: #{w[:type]}\\n"
+      lines << "➡️ Weight: #{weight}\\n"
+      lines << "➡️ Sets x Reps: #{w[:sets]} x #{w[:reps]}\\n"
+      lines << "\\n"
+    end
+
+    lines << "💪 Have a great session!"
+
+    lines.join("")
+  end
+end
+
+
+workout = WorkoutGenerator.new(1)
+workout.generate_workout
+
+ICSGenerator.generate(workout)
+puts "\nCalendar file 'olympic_lifting_program.ics' has been created."
+puts "Import this file into your calendar application."
